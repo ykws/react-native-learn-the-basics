29681454b1f2a517645308741181441ceeafbc38
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2020-02-25 23:41:50 +0900

    Props

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-export default class HelloWorldApp extends Component {
+class Greeting extends Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello, world!</Text>
+      <View style={{alignItems: 'center'}}>
+        <Text>Hello {this.props.name}!</Text>
+      </View>
+    );
+  }
+}
+
+export default class LotsOfGreetings extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center', top: 50}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
       </View>
     );
   }
