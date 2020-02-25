b3c0e00f0668935bcf451f289a40915cb05f5736
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2020-02-25 23:58:14 +0900

    Style

--- App.js
+++ App.js
@@ -1,37 +1,14 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-class Blink extends Component {
-
-  componentDidMount() {
-    setInterval(() => (
-      this.setState(previousState => (
-        { isShowingText: !previousState.isShowingText }
-      ))
-    ), 1000);
-  }
-
-  state = { isShowingText: true}
-
-  render() {
-    if (!this.state.isShowingText) {
-      return null;
-    }
-
-    return (
-      <Text>{this.props.text}</Text>
-    );
-  }
-}
-
-export default class BlinkApp extends Component {
+export default class LotsOfStyles extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Blink text='I love to blink' />
-        <Blink text='Yes blinking is so great' />
-        <Blink text='Why did they ever take this out of HTML' />
-        <Blink text='Look at me look at me look at me' />
+        <Text style={styles.red}>just red</Text>
+        <Text style={styles.bigBlue}>just bigBlue</Text>
+        <Text style={styles.bigBlue, styles.red}>bigBlue, then red</Text>
+        <Text style={styles.red, styles.bigBlue}>red, then bigBlue</Text>
       </View>
     );
   }
@@ -43,5 +20,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
   },
 });
